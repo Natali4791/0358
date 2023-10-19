403fa3b5c5d09924f031de6c202b2e43e209f154
Natali4791 <148436114+Natali4791@users.noreply.github.com>
2023-10-19 18:45:09 +0300

    Добавлено внесение по буквам на главной

--- Bot/bot.js
+++ Bot/bot.js
@@ -16,20 +16,28 @@ let keywords = ["10 самых популярных шрифтов от Google",
                 "Отключение редакций и ревизий в WordPress", 
                 "Вывод произвольных типов записей и полей в WordPress"];
 let keyword = keywords[getRandom(0, keywords.length)];
-
+//Работаем на главной странице
 if (searchBtn !== null) {
-  input.value = keyword;
-  searchBtn.click();
-} else {
-  for (let i = 0; i < links.length; i++) {
-    if (links[i].href.includes("napli.ru")) {
-      let link = links[i];
-      console.log("Нашел строку!" + link);
-      link.click();
-      break;
+  let i = 0;
+  let timerId = setInterval(() => {
+    input.value +=keyword[i];
+    i++;
+    if (i == keyword.length) {
+      clearInterval(timerId);
+      searchBtn.click();
+    }
+  },500)
+//Работаем на странице поисковой выдачи
+  } else {
+    for (let i = 0; i < links.length; i++) {
+      if (links[i].href.includes("napli.ru")) {
+        let link = links[i];
+        console.log("Нашел строку!" + link);
+        link.click();
+        break;
+      }
     }
   }
-}
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 } 
